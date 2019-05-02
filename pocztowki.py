bd59a451aff751e9a774a785baf20ffe83e88f5e
Paweł Piątkowski <piatkowskip@gmail.com>
2019-05-02 18:33:17 +0200

    Logging and output expected image size

--- pocztowki.py
+++ pocztowki.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import logging
 from PIL import Image
 
 
@@ -12,11 +13,13 @@ class Size:
         return float(self.width) / self.height
 
     def print(self, name, unit):
-        print("%s size %d%s x %d%s (ratio=%f)" %
-              (name, self.width, unit, self.height, unit, self.ratio()))
+        logging.info("%s size %d%s x %d%s (ratio=%f)" %
+                     (name, self.width, unit, self.height, unit, self.ratio()))
 
 
 def main(argv):
+    logging.basicConfig(level=logging.INFO)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("image")
     parser.add_argument("-m", "--mode", required=False, default="aspectFill")
@@ -33,13 +36,13 @@ def main(argv):
     try:
         paperSize = Size(int(args.paper_width), int(args.paper_height))
         imageSize = Size(int(args.image_width), int(args.image_height))
-        calculateExpandedImage(args.image, paperSize, imageSize, args.mode)
+        createOutputImage(args.image, paperSize, imageSize, args.mode)
     except FileNotFoundError:
+
         print("File not found")
 
 
-def calculateExpandedImage(filename, paperSize, imageSize, mode):
-    print("mode: %s" % (mode))
+def createOutputImage(inputFilename, paperSize, imageSize, mode):
     paperSize.print("PAPER", "mm")
     imageSize.print("IMAGE", "mm")
 
@@ -53,10 +56,10 @@ def calculateExpandedImage(filename, paperSize, imageSize, mode):
     else:
         raise ValueError("Invalid mode")
 
-    print("I/P w=%f h=%f targetRatio=%f" %
-          (widthRatio, heightRatio, targetRatio))
+    logging.debug("I/P w=%f h=%f targetRatio=%f" %
+                  (widthRatio, heightRatio, targetRatio))
 
-    image = Image.open(filename)
+    image = Image.open(inputFilename)
     inputImageSize = Size(int(image.width), int(image.height))
     inputImageSize.print("INPUT IMAGE", "px")
 
@@ -64,14 +67,19 @@ def calculateExpandedImage(filename, paperSize, imageSize, mode):
                            int(inputImageSize.height / targetRatio))
     outputImageSize.print("OUTPUT_IMAGE", "px")
 
+    expectedImageSize = Size(
+        paperSize.width * targetRatio, paperSize.height * targetRatio)
+    logging.info("Expected output image size: %dmm x %dmm. Height difference: %d mm" %
+                 (expectedImageSize.width, expectedImageSize.height, expectedImageSize.height - imageSize.height))
+
     newSize = (outputImageSize.width, outputImageSize.height)
     outputImage = Image.new("RGB", newSize, (255, 255, 255))
 
     outputImage.paste(image, (int((newSize[0] - inputImageSize.width)/2),
                               int((newSize[1] - inputImageSize.height)/2)))
-    outputFilename = "out_" + filename
+    outputFilename = "out_" + inputFilename
     outputImage.save(outputFilename)
-    print("Output image saved to %s" % (outputFilename))
+    logging.info("Output image saved to %s" % (outputFilename))
 
 
 if __name__ == "__main__":
